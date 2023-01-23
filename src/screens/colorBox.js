3da56f2b151d9e569b6aafc919f2ff8c63c949aa
KIKVA03 <datokikvadze8@gmail.com>
2023-01-23 16:05:12 +0400

    useing useReducer

--- src/screens/colorBox.js
+++ src/screens/colorBox.js
@@ -1,32 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { View, Button, Text } from "react-native";
 import ChangeColorComponent from "../components/changeColorComponent";
 
+const changeAmount = 25;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "red":
+            return { ...state, red: state.red + action.amount };
+
+        case "green":
+            return { ...state, green: state.green + action.amount };
+
+        case "blue":
+            return { ...state, blue: state.blue + action.amount };
+    }
+};
+
 const ColorBox = () => {
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+
+    const { red, green, blue } = state;
+
     return (
         <View>
             <ChangeColorComponent
-                increase={() => setRed(red + 30)}
-                decrease={() => setRed(red - 30)}
+                increase={() => dispatch({ type: "red", amount: changeAmount })}
+                decrease={() => dispatch({ type: "red", amount: -1 * changeAmount })}
                 color="red"
             />
             <ChangeColorComponent
-                increase={() => setGreen(green + 30)}
-                decrease={() => setGreen(green - 30)}
+                increase={() => dispatch({ type: "green", amount: changeAmount })}
+                decrease={() => dispatch({ type: "green", amount: -1 * changeAmount })}
                 color="green"
             />
             <ChangeColorComponent
-                increase={() => setBlue(blue + 30)}
-                decrease={() => setBlue(blue - 30)}
-                color="blue"
+                increase={() => dispatch({ type: "blue", amount: changeAmount })}
+                decrease={() => dispatch({ type: "blue", amount: -1 * changeAmount })}
             />
             <View
                 style={{
-                    width: 100,
-                    height: 100,
+                    width: 200,
+                    height: 200,
                     backgroundColor: ` rgb(${red}, ${green}, ${blue})`,
                 }}
             ></View>
